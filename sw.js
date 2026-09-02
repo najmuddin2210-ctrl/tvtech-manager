db9d495e905d67630cedffffff696563e635ee74
najmuddin2210-ctrl <najmuddin2210@gmail.com>
2026-09-03 04:02:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='seentv-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
+const C='seentv-v2-1';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])))});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim().then(()=>caches.keys()).then(a=>Promise.all(a.filter(x=>x!==C).map(x=>caches.delete(x)))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
